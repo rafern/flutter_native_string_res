699c69eaf7c3d63ad4e1847914151a57e820086d
trunghieuvn <hieu.trantrung1204@gmail.com>
2024-04-10 16:24:15 +0700

    Fix run flutter 3.19.5

---
+++ example/android/app/src/main/java/com/flutter/flutter_native_string_res_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter.flutter_native_string_res_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/trantrunghieu/DevTools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/Realtek/opensource/flutter_native_string_res/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Volumes/Realtek/opensource/flutter_native_string_res/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lNzZjOTU2NDk4ODQxZTFhYjQ1ODU3N2QzODkyMDAzZTU1M2U0ZjNjLw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Volumes/Realtek/opensource/flutter_native_string_res/example/.dart_tool/package_config.json"
